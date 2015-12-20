b907e4fcb87d5ad1e5362e3559fd9cab79d81156
kroovy <kroovy@web.de>
2015-12-20 13:36:46 +0000

    commented in another function in main

---
+++ include/minecraft.h
@@ -0,0 +1,11 @@
+#ifndef MINECRAFT_H
+#define MINECRAFT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+void print_to_mc(void);
+void create_block(char *cmd);
+char * set_block_coords(int x1, int y1, int z1, int x2, int y2, int z2, char block[]);
