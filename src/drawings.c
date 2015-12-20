940802ca73040853e1ade453fd8379a2ade5e977
kroovy <kroovy@web.de>
2015-12-20 18:25:23 +0000

    commented in another function in main

--- src/drawings.c
+++ src/drawings.c
@@ -2,11 +2,11 @@
 
 void draw_square(void)
 {
-    for (int y = 0; y < 50; y++) 
+    for (int y = 0; y < 10; y++) 
     {
-        for (int x = 0; x < 50; x++) 
+        for (int x = 0; x < 10; x++) 
         {
-            create_block(set_block_coords(x, 255, y, x, 255, y, "diamond_ore"));
+            create_block(set_block_coords(x, 255, y, x, 255, y, "dirt"));
         }
     }
 }
