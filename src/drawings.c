b35394b7a894ae33eab880fe2293ce33933e59f3
kroovy <kroovy@web.de>
2015-12-20 17:23:50 +0000

    commented in another function in main

--- src/drawings.c
+++ src/drawings.c
@@ -2,9 +2,9 @@
 
 void draw_square(void)
 {
-    for (int y = 0; y < 5; y++) 
+    for (int y = 0; y < 50; y++) 
     {
-        for (int x = 0; x < 5; x++) 
+        for (int x = 0; x < 50; x++) 
         {
             create_block(set_block_coords(x, 0, y, x, 0, y, "dirt"));
         }
