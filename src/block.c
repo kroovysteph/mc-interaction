837a4d9d88b71fc1fe89c9946a4e83d6ed1e4ec8
kroovy <kroovy@web.de>
2015-12-20 14:54:23 +0000

    commented in another function in main

--- src/block.c
+++ src/block.c
@@ -27,7 +27,7 @@ char * set_block_coords(int x1, int y1, int z1, int x2, int y2, int z2, char blo
 		cmd[i] = ' ';
 	}
 	
-	cmd[] = "tmux send-keys -t mc console \"/fill ";
+	cmd = "tmux send-keys -t mc console \"/fill ";
 	
 	//Koordinaten mit Offset
 	x1 = (-91 - x1);
